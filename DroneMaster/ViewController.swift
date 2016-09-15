4c43c43dda395ec24d14ce29a4d1047f7085381e
Rean Wen <rean.wen@gmail.com>
2016-09-15 15:46:34 -0400

    0.1

--- DroneMaster/ViewController.swift
+++ DroneMaster/ViewController.swift
@@ -21,6 +21,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func connectToDroneAct(_ sender: AnyObject) {
+        let header = ["x-commander-email":"hello@gmail.com"]
+        Alamofire.request("http://challenge2.airtime.com:10001/start", method: .get, parameters: [:], encoding: URLEncoding.default, headers: header)
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                    let firstDict = self.convertStringToDictionary(text: String(describing: JSON))
+                    
+                }
+                
+        }
+    }
+    
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.data(using: String.Encoding.utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject]
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        return nil
+    }
 }
 
